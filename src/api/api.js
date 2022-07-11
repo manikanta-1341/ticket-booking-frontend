71cfb8f067522001645ae662a6e4db9771b44b06
manikanta1341 <medicharlamanikanta13@gmail.com>
2022-07-11 20:55:40 +0530

    first commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 
 
-export const url = "http://localhost:5000"
-export const frontend_url = "http://localhost:3000"
+export const url = "https://redux-ticket-booking-app.herokuapp.com"
+export const frontend_url = "https://redux-ticket-booking-app.netlify.app"
 
